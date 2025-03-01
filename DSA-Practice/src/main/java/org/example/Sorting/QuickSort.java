698ad99d01c4dd0c1e64aebab5b14396a5412dd4
Pranav Laygude <123pranavlaygude@gmail.com>
2025-03-02 00:23:59 +0530

    update 1.4

---
+++ DSA-Practice/src/main/java/org/example/Sorting/QuickSort.java
@@ -0,0 +1,11 @@
+package org.example.Sorting;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void quickSort(int[] arr, int s, int p, int e){
+        
+    }
+}
