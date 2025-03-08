faf2ac72724d7c02327fb553ba69304176f65fee
Pranav Laygude <123pranavlaygude@gmail.com>
2025-03-08 20:25:46 +0530

    update 1.7

--- DSA-Practice/src/main/java/org/example/Strings/Stream.java
+++ DSA-Practice/src/main/java/org/example/Strings/Stream.java
@@ -3,6 +3,7 @@ package org.example.Strings;
 public class Stream {
     public static void main(String[] args) {
         skip("", "bAscdahsaA");
+        System.out.println(skip("bAscdahsaApppp"));
     }
 
     public static void skip(String p, String up){
@@ -17,6 +18,21 @@ public class Stream {
             skip(p, up.substring(1));
         }else {
             skip(p+ch, up.substring(1));
+        }
+
+    }
+
+    public static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a' || ch == 'A'){
+            return skip(up.substring(1));
+        }else {
+            return ch + skip(up.substring(1));
         }
 
     }
